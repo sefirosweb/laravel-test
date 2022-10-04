64f7fa0a328bdd4d33bf40fdf77e20e2061426df
Javier García Fillol <javier.garcia@westwing.es>
2022-10-04 21:43:58 +0200

    Added Laravel Access List

--- config/app.php
+++ config/app.php
@@ -196,6 +196,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Sefirosweb\LaravelMailing\LaravelMailingServiceProvider::class,
+        Sefirosweb\LaravelAccessList\LaravelAccessListServiceProvider::class,
 
     ],
 
