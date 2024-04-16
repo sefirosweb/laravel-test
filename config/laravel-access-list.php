252a9b68d145e667bc5795c3b0e54220c14031e9
Sefirosweb <sefirosweb@gmail.com>
2024-04-16 10:10:40 +0000

    Changed example validation to final model

--- config/laravel-access-list.php
+++ config/laravel-access-list.php
@@ -2,5 +2,8 @@
 
 return [
     'prefix' => 'acl',
-    'middleware' => ['web']
+    'middleware' => ['web'],
+    'AccessList' => Sefirosweb\LaravelAccessList\Http\Models\AccessList::class,
+    'Role' => Sefirosweb\LaravelAccessList\Http\Models\Role::class,
+    'User' => App\Models\User::class,
 ];
